a571f7c4f22b4c30fde5260b8f22f3a42acaeb11
kamalogudah <mcpaul2058@gmail.com>
2019-10-13 23:36:55 +0300

    Writing and Running a Test

--- test/GradeBook.Tests/UnitTest1.cs
+++ test/GradeBook.Tests/UnitTest1.cs
@@ -3,12 +3,22 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
-    public class UnitTest1
+  public class UnitTest1
+  {
+    [Fact]
+    public void Test1()
     {
-        [Fact]
-        public void Test1()
-        {
+      // arrange
+      var x = 5;
+      var y = 2;
+      var expected = 7;
 
-        }
+      // act
+      var actual = x + y;
+
+      // assert
+
+      Assert.Equal(expected, actual);
     }
+  }
 }
