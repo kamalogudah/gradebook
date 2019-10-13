afea3f05f54a775496ee5f12f00ea97c5e11a68f
kamalogudah <mcpaul2058@gmail.com>
2019-10-13 23:51:05 +0300

    Referencing Projects and Packages

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -3,22 +3,26 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
-  public class UnitTest1
+  public class BookTests
   {
     [Fact]
     public void Test1()
     {
       // arrange
-      var x = 5;
-      var y = 2;
-      var expected = 7;
+      var book = new Book("");
+      book.AddGrade(89.1);
+      book.AddGrade(90.5);
+      book.AddGrade(77.3);
+
+
 
       // act
-      var actual = x + y;
+      book.ShowStatistics();
+
 
       // assert
 
-      Assert.Equal(expected, actual);
+
     }
   }
 }
