d5f652d69c7d934132eff0575fb3b0da09782766
kamalogudah <mcpaul2058@gmail.com>
2019-10-21 02:57:29 +0300

    Referencing Different Objects

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,14 +6,28 @@ namespace GradeBook.Tests
   public class TypeTests
   {
     [Fact]
-    public void Test1()
+    public void GetBookReturnsDifferentObjects()
     {
       // arrange
       var book1 = GetBook("Book 1");
       var book2 = GetBook("Book 2");
 
-      Assert.Equal("", book1.Name);
-      Assert.Equal("", book2.Name);
+      Assert.Equal("Book 1", book1.Name);
+      Assert.Equal("Book 2", book2.Name);
+      Assert.NotSame(book1, book2);
+
+    }
+
+    [Fact]
+    public void TwoVariablesCanReferenceSameObject()
+    {
+      // arrange
+      var book1 = GetBook("Book 1");
+      var book2 = book1;
+      // Assert.Equal("Book 1", book1.Name);
+      // Assert.Equal("Book 1", book2.Name);
+      Assert.Same(book1, book2);
+      Assert.True(Object.ReferenceEquals(book1, book2));
 
     }
     Book GetBook(string name)
